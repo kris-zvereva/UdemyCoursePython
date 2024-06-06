1e465356c942f0af48491142e9f85a8cc9ffb938
kris-zvereva <krs.zvrv@gmail.com>
2024-06-07 00:09:13 +0200

    Initial commit

---
+++ 01 variables/code.py
@@ -0,0 +1,29 @@
+# -- Numbers --
+
+x = 15
+price = 9.99
+
+discount = 0.2
+
+result = price * (1 - discount)
+print(result)
+
+# -- Strings --
+
+name = "Rolf"
+
+print(name)
+print(name * 2)
+
+# -- Changing variables --
+
+a = 25
+b = a
+
+print(a)
+print(b)
+
+b = 17
+
+print(a)
+print(b)
---
+++ 02 string formatting/code.py
@@ -0,0 +1,8 @@
+name = "Bob"
+greeting = f"Hello, {name}"
+
+print(f"Hello, {name}")
+
+name = "Rolf"
+print(f"Hello, {name}")
+
