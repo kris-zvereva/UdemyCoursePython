a05c6a5c151e830698ac7e4a4cec79f3a8170aab
kris-zvereva <krs.zvrv@gmail.com>
2024-06-07 01:34:32 +0200

    progress

--- 02 string formatting/code.py
+++ 02 string formatting/code.py
@@ -1,3 +1,5 @@
+# -- String formatting
+
 name = "Bob"
 greeting = f"Hello, {name}"
 
@@ -5,4 +7,18 @@ print(f"Hello, {name}")
 
 name = "Rolf"
 print(f"Hello, {name}")
+
+name = "Kris"
+greeting = "Hello, {}"
+with_name = greeting.format(name)
+with_name2 = greeting.format("Rob")
+
+print(with_name)
+print(with_name2)
+
+longer_phrase = "Hello, {}. Today is {}"
+formatted = longer_phrase.format("Jane", "Thursday")
+print(formatted)
+
+
 
