1c88db7b01f0815e6d44d29aadb71725d64532a9
kris-zvereva <krs.zvrv@gmail.com>
2024-06-10 00:04:32 +0200

    progress

--- 18 functions returning values/code.py
+++ 18 functions returning values/code.py
@@ -47,3 +47,17 @@ print(result)  # 5
 another = divide(15, 0)
 print(another)  # You fool!
 
+
+# exercise
+# Complete the function by making sure it returns 42. .
+def return_42():
+    return 42
+    # Complete function here
+    # 'pass' just means "do nothing". Make sure to delete this!
+
+
+# Create a function below, called my_function,
+# that takes two arguments and returns the result of its two arguments multiplied together.
+def my_function(x, y):
+    return x*y
+
