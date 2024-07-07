9995db0a0bdd5653863a6870b9b5564c8449e0c8
kris-zvereva <krs.zvrv@gmail.com>
2024-07-07 14:52:34 +0200

    progress

---
+++ 33 first class functions/code.py
@@ -0,0 +1,73 @@
+def divide(dividend, divisor):
+    if divisor == 0:
+        raise ZeroDivisionError("divisor cant be 0")
+
+    return dividend / divisor
+
+
+def calculate(*values, operator):
+    return operator(*values)
+
+
+result = calculate(20, 4, operator=divide)
+print(result)
+
+
+def search(sequence, expected, finder):
+    for elem in sequence:
+        if finder(elem) == expected:
+            return elem
+    raise RuntimeError(f"Could not find an element with {expected}")
+
+
+friends = [
+    {"name": "Rolf Smith", "age": 24},
+    {"name": "Adam Wool", "age": 30},
+    {"name": "Anne Pun", "age": 27},
+]
+
+
+def get_friend_name(friend):
+    return friend["name"]
+
+
+print(search(friends, "Anne Pun", get_friend_name))
+
+# -- using lambdas since this can be simple enough --
+
+
+def search(sequence, expected, finder):
+    for elem in sequence:
+        if finder(elem) == expected:
+            return elem
+    raise RuntimeError(f"Could not find an element with {expected}")
+
+
+friends = [
+    {"name": "Rolf Smith", "age": 24},
+    {"name": "Adam Wool", "age": 30},
+    {"name": "Anne Pun", "age": 27},
+]
+
+print(search(friends, "Bob Smith", lambda friend: friend["name"]))
+
+
+# -- or as an extra, using built-in functions --
+
+from operator import itemgetter
+
+
+def search(sequence, expected, finder):
+    for elem in sequence:
+        if finder(elem) == expected:
+            return elem
+    raise RuntimeError(f"Could not find an element with {expected}")
+
+
+friends = [
+    {"name": "Rolf Smith", "age": 24},
+    {"name": "Adam Wool", "age": 30},
+    {"name": "Anne Pun", "age": 27},
+]
+
+print(search(friends, "Rolf Smith", itemgetter("name")))
