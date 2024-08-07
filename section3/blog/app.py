52c18545d0897f59b3b6334ae90a9fd23d4690ed
kris-zvereva <krs.zvrv@gmail.com>
2024-08-07 21:59:01 +0200

    progress on sec3

--- section3/blog/app.py
+++ section3/blog/app.py
@@ -1,3 +1,5 @@
+MENU_PROMPT = 'Enter "c" to create a blog, "l" to list blogs, "r" to read one, "p" to create a post, or "q" to quit: '
+# normally means a constant, but upper case doesnt make it unchangeable
 blogs = dict()  # blog_name : blog object
 
 def menu():
@@ -6,10 +8,10 @@ def menu():
 # do smt with that choice
 # exit
     print_blogs()
+    selection = input(MENU_PROMPT)
 
 
 def print_blogs():
     for key, blog in blogs.items():  # key = title
         print('- {}'.format(blog))
 
-        
--- section3/blog/tests/system/test_app.py
+++ section3/blog/tests/system/test_app.py
@@ -5,9 +5,21 @@ from section3.blog.blog import Blog
 
 
 class AppTest(TestCase):
+    def test_menu_prints_prompt(self):
+        with patch('builtins.input') as mocked_input:
+            app.menu()
+            mocked_input.assert_called_with(app.MENU_PROMPT)
+
+    def test_menu_calls_print_blogs(self):
+        with patch('app.print_blogs') as mocked_print_blogs:
+            with patch('builtins.input', return_value='q'):
+                app.menu()
+                mocked_print_blogs.assert_called()
+
     def test_print_blog(self):
         blog = Blog('Test', 'Test Author')
         app.blogs = {'Test': blog}
         with patch('builtins.print') as mocked_print:
             app.print_blogs()
             mocked_print.assert_called_with('- <Blog(Test, Test Author, posts: 0)>')
+
