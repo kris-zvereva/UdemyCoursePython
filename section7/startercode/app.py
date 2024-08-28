8f5d0df3eb67e07fe6d27befde1356f71a824358
kris-zvereva <krs.zvrv@gmail.com>
2024-08-28 22:59:44 +0200

    final with section 7

--- section7/startercode/app.py
+++ section7/startercode/app.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     db.init_app(app)
 
     if app.config['DEBUG']:
-        @app.before_first_request
+        #@app.before_first_request
         def create_tables():
             db.create_all()
 
--- section7/startercode/models/user.py
+++ section7/startercode/models/user.py
@@ -5,8 +5,8 @@ class UserModel(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80))
-    password = db.Column(db.String())
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(), nullable=False)
 
     def __init__(self, username, password):
         self.username = username
